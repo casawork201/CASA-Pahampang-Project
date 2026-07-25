ac4585b4187dcef30729f381a1332c808da37583
Joepril Sobrevega <casawork201@gmail.com>
2026-07-25 11:44:54 +0800

    Added nuget config

--- CASAPahampang.Client/obj/Debug/net10.0/CASAPahampang.Client.AssemblyInfo.cs
+++ CASAPahampang.Client/obj/Debug/net10.0/CASAPahampang.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CASAPahampang.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b55b03393e29be10249acfe170b697213c7e63e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a86d8dd6fd8b19a8b25507c1487a39168c6047d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CASAPahampang.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CASAPahampang.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
