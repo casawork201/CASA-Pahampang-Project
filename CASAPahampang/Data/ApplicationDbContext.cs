bfaa8cdb30b547d648ce6b49daf5c3e6da2bd04f
Joepril Sobrevega <casawork201@gmail.com>
2026-07-28 11:35:16 +0800

    Added an ApplicationDbContext

---
+++ CASAPahampang/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CASAPahampang.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CASAPahampang.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+    {
+        
+    }
+    public DbSet<Team> Teams { get; set; }
+    public DbSet<Match> Matches { get; set; }
+    public DbSet<ChatMessage> ChatMessages { get; set; }
+    public DbSet<AvatarOption> AvatarOptions { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
