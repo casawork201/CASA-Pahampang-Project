cb12f77cd37590dc9d14716b522943d9013cab65
Joepril Sobrevega <casawork201@gmail.com>
2026-08-10 10:54:24 +0800

    Updated the Navigation for both Admin and Users

--- CASAPahampang.Client/obj/Debug/net10.0/CASAPahampang.Client.AssemblyInfo.cs
+++ CASAPahampang.Client/obj/Debug/net10.0/CASAPahampang.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CASAPahampang.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18ab12a191c648f9a7304506fef8b9c2072ef37b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c72407908891970d6c1d419e6c0d91e300f1f203")]
 [assembly: System.Reflection.AssemblyProductAttribute("CASAPahampang.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CASAPahampang.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
