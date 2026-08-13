36f9165a0f5b55fb93196b62b44c2ab91c317959
Joepril Sobrevega <casawork201@gmail.com>
2026-08-13 16:48:22 +0800

    Modify the following Pages for GUI's resposiveness - BingoDisplay - Scorekeeper

--- CASAPahampang/obj/Debug/net10.0/CASAPahampang.AssemblyInfo.cs
+++ CASAPahampang/obj/Debug/net10.0/CASAPahampang.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CASAPahampang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42c98407b56d82bb309c721193c8d4a4252dfd8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b692b30f1414e98a8d0e6a2eda5c6fa4a25d00db")]
 [assembly: System.Reflection.AssemblyProductAttribute("CASAPahampang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CASAPahampang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
